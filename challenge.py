e626dbf0d105ec173e0299beae78e89cd24eeab3
hpmouton <hubertthedev@gmail.com>
2021-10-28 18:42:35 +0200

    deployment version

--- challenge.py
+++ challenge.py
@@ -2,8 +2,9 @@ import streamlit as st
 import pandas as pd
 
 st.write(""" # IndabaX Hackathon 2021 """)
+st.write(""" ## Team Innate """)
 
 df = pd.read_csv("./dataset.csv")
-st.title("Hello world!")
+st.title("Sample Data")
 
 st.write(df.head())
