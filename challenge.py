c41523ade06193796390bff641136068bd7858a8
hpmouton <hubertthedev@gmail.com>
2021-10-28 18:49:01 +0200

    deployment version

--- challenge.py
+++ challenge.py
@@ -7,7 +7,7 @@ st.write(""" # IndabaX Hackathon 2021 """)
 st.write(""" ## Team Innate """)
 
 df = pd.read_csv("./dataset.csv")
-sorted_data = df.sort_values('DATE')
+sorted_data = df.sort_values('DATE',ascending=True)
 st.title("Sample Data")
 
 st.write(sorted_data.head())
