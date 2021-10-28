93076a38aec5d6301bf5e37fd0c7ca38caed2d5a
hpmouton <hubertthedev@gmail.com>
2021-10-28 19:23:05 +0200

    deployment version

--- challenge.py
+++ challenge.py
@@ -3,16 +3,19 @@ import pandas as pd
 import datetime 
 
 
-
 st.write(""" # IndabaX Hackathon 2021 """)
 st.write(""" ## Team Innate """)
 
 df = pd.read_csv("./dataset.csv")
 sorted_data = df.sort_values('DATE',ascending=True)
 st.title("Sample Data")
-
-
 st.write(sorted_data.head(20))
+#Dataframe for Installed_Preactive SDP_Status
+status = "INSTALLED_PREACTIVE"
+new_users_df = df[df['SDP_STATUS'].isin(status)] 
+st.title("New User Data")
+st.write(new_users_df.head(20))
 
 n_years = st.slider('Years of prediction:', 1, 4)
 period = n_years * 365
+
