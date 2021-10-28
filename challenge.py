558a32da770a325f497dc22e6c21855772e9f734
hpmouton <hubertthedev@gmail.com>
2021-10-28 18:47:46 +0200

    deployment version

--- challenge.py
+++ challenge.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import pandas as pd
+import datetime 
+
 
 st.write(""" # IndabaX Hackathon 2021 """)
 st.write(""" ## Team Innate """)
 
 df = pd.read_csv("./dataset.csv")
+sorted_data = df.sort_values('DATE')
 st.title("Sample Data")
 
-st.write(df.head())
+st.write(sorted_data.head())
